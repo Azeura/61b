f7066afa2896ab3234958bfdb7a2cae46483bef0
liosa <selqulineliot@gmail.com>
2025-10-13 16:32:47 +0800

    compilation error fix fourth times

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -67,10 +67,6 @@ public class Main {
                 Repository.showFiles();
                 break;
         }
-    } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
-            System.exit(1);
-    }
-
+    } 
     }
 }
