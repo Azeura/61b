ee6efbb839e7ed8cce709d939dcb8ab6cf38953b
liosa <selqulineliot@gmail.com>
2025-10-13 16:19:32 +0800

    compilation error fix

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -10,13 +10,14 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args)  {
         // TODO: what if args is empty?
         if ( args.length == 0) {
             System.out.println("Please enter a command.");
             System.exit(0);
         }
         String firstArg = args[0];
+        try {
         switch(firstArg) {
             case "init":
                 Repository.setupPersistency();
@@ -66,5 +67,10 @@ public class Main {
                 Repository.showFiles();
                 break;
         }
+    } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
+    }
+
     }
 }
